f5a2164928a885e7437d32bfe3783ea96830e8a2
falcon 草 <falcon.lee1011@gmail.com>
2021-03-05 09:30:11 +0800

    Initial Commit

---
+++ hw-guessing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw-guessing
+//
+//  Created by falcon on 2021/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw-guessing/hw_guessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw_guessingApp.swift
+//  hw-guessing
+//
+//  Created by falcon on 2021/3/5.
+//
+
+import SwiftUI
+
+@main
+struct hw_guessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
